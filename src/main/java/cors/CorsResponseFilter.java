0ce80846566fbcdfa851e7874c9655c1bb6b31d3
Martin <cph-ms782@cphbusiness.dk>
2020-01-12 21:13:29 +0100

    CORS update

--- src/main/java/cors/CorsResponseFilter.java
+++ src/main/java/cors/CorsResponseFilter.java
@@ -17,9 +17,9 @@ public class CorsResponseFilter implements ContainerResponseFilter {
   public void filter( ContainerRequestContext requestCtx, ContainerResponseContext res )
     throws IOException {
     // LOG.info( "Executing REST response filter" );
-//    res.getHeaders().add("Access-Control-Allow-Origin", "*" );
-    res.getHeaders().add("Access-Control-Allow-Origin", "https://www.sandersoft.dk/exam" );
-    res.getHeaders().add("Vary", "Origin" );
+    res.getHeaders().add("Access-Control-Allow-Origin", "*" );
+//    res.getHeaders().add("Access-Control-Allow-Origin", "https://www.sandersoft.dk" );
+//    res.getHeaders().add("Vary", "Origin" );
     res.getHeaders().add("Access-Control-Allow-Credentials", "true" );
     res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
     res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization,x-access-token");
